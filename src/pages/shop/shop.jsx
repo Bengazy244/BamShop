e2fdcd14b2597816e44cfa9f7f157ae1c7226d29
Benjamin Badu-Koomson <bengazy244@gmail.com>
2024-12-06 07:23:15 +0000

    Added the search bar functionality

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,32 +1,3 @@
-// import { PRODUCTS } from "../../products";
-// import { Product } from "./product";
-
-// import "./shop.css";
-// import "../../App.css";
-
-// export const Shop = () => {
-//   return (
-//     <div className="shop">
-//       <div className="container">
-//         <div className="searchbar">
-//           <input
-//             type="text"
-//             name="search"
-//             className="search"
-//             placeholder="Search products..."
-//           />
-//         </div>
-
-//         <div className="products">
-//           {" "}
-//           {PRODUCTS.map((product, index) => (
-//             <Product data={product} key={index} />
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 import { useState } from "react"; // Import useState for managing state
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
